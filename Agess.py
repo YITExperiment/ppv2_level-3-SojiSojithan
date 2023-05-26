d1de3d7aede60cae16c1d7c59a494b31152bac8b
Sojithan <134693439+SojiSojithan@users.noreply.github.com>
2023-05-26 19:24:53 +0530

    Add files via upload

---
+++ Agess.py
@@ -0,0 +1,11 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=15
+>>> print(age)
+15
+>>> weight=55
+>>> print(weight)
+55
+>>> height=154
+>>> print(height)
+154
